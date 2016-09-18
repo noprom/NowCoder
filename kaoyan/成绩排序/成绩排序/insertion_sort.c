1c3083e51bbf4053570dba5e6e3cb37d938ecc46
noprom <tyee.noprom@qq.com>
2016-09-18 17:18:42 +0800

    rm c files

---
+++ kaoyan/成绩排序/成绩排序/insertion_sort.c
@@ -0,0 +1,9 @@
+//
+//  insertion_sort.c
+//  成绩排序
+//
+//  Created by noprom on 9/18/16.
+//  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
+//
+
+#include "insertion_sort.h"
---
+++ kaoyan/成绩排序/成绩排序/insertion_sort.h
@@ -0,0 +1,14 @@
+//
+//  insertion_sort.h
+//  成绩排序
+//
+//  Created by noprom on 9/18/16.
+//  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
+//
+
+#ifndef insertion_sort_h
+#define insertion_sort_h
+
+#include <stdio.h>
+
+#endif /* insertion_sort_h */
